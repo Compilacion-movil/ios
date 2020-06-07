6cd54dac2f7a7a2a5a9aded52401bbbc151a4724
antocara <antocara@gmail.com>
2020-06-07 18:18:15 +0200

    ejemplo variables en Swift

---
+++ ejemplo de variables/variables.playground/Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+//ejemplos de variables
+var hello: String = "Hola"
+var age: Int = 20
+var isCorrect = true
+
+
+//Declara implícitamente el tipo
+var name: String = "Ana"
+
+//Dejar que el compilador infiera el tipo
+var carsNumbers = 2
+
+//esta última declaración es igual que
+var carsNumbers: Int = 2
+
+//Error al no declarar un valor inicial a la
+//variable
+var incorrectVar
+
+//Una variable solo puede tener un tipo de datos
+var carName = "Toyota"
+carName = 2
+
+//Asignación de tipos explícito para
+//eviatr errores al no concoer el tipo exacto
+var price:Float = 8.9
+var priceDouble: Double = 8.9
+
+
+
+if (price == priceDouble){
+    //No son del mimso tipo y el compilador
+    //nos avisará
+}
+
+print(price)
+
+//Constantes y variables
+//esto es una constante
+let color: String = "Azul"
+let color = "Amarillo"
+
+//cambiamos el valor de una variable
+var colorName = "Azul"
+print(colorName)
+colorName = "Amarillo"
+print(colorName)
+
+
+
+//Nombres de variables
+
+var estenombre no es correcto = ""
+var >miVariable = ""
+var 2Coches = 2
+var cocheNumber2 = 2
